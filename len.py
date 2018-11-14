8f3019375b96319c872153a3cdf393f346453cc1
sergey@kletsko <sergey@kletsko.com>
2018-11-14 13:27:47 -0600

    New file

--- len.py
+++ len.py
@@ -1,1 +1,7 @@
-len("hello")
+character_name = "Sergey"
+character_age = "50"
+
+print("There once was a man named " + character_name + ", ")
+print("There once was a man named John, ")
+print("He was " + character_age + " years old. ")
+print("He was 35 years old. ")
